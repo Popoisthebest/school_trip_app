d7a6a9c9c49a1f3a0fc741fcf751ca77e5f6a256
Popo <147984101+Popoisthebest@users.noreply.github.com>
2024-10-03 17:40:39 +0900

    [ui] Spalsh 페이지 구현 완료

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,50 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:school_trip_app/screens/main_screen.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+
+    // 시스템 UI를 immersive 모드로 설정하여 상태바와 내비게이션 바를 숨김
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    // 2초 후에 MainScreen으로 화면 전환
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(
+        PageRouteBuilder(
+          pageBuilder: (context, animation, secondaryAnimation) =>
+              const MainScreen(),
+          // 화면 전환 시 FadeTransition을 사용하여 부드러운 페이드 애니메이션 효과를 적용
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation, // 애니메이션의 투명도 조절
+              child: child,
+            );
+          },
+        ),
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    // 화면이 사라질 때 시스템 UI 모드를 원래 상태로 되돌림 (상태바와 내비게이션 바 표시)
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,12 +56,13 @@ class SplashScreen extends StatelessWidget {
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          // 로고 이미지 표시
           Image.asset('assets/logo/mydashintrip_LOGO.png',
               width: 100, height: 100),
           const Text(
             'MyDaeShinTrip',
             style: TextStyle(
-              fontFamily: "Ownglyph okticon", // 미정
+              fontFamily: "Ownglyph okticon", // 폰트 설정(미정)
               fontSize: 24,
               fontWeight: FontWeight.w400,
               color: Color(0xff000000),
