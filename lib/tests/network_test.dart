880fb344aa462c09cd1910603ea5b9f395be08c5
Popo <147984101+Popoisthebest@users.noreply.github.com>
2024-10-03 14:29:30 +0900

    [feat] 네트워크 연결 확인 기능 추가

---
+++ lib/tests/network_test.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:school_trip_app/services/network_connectivity_observer.dart';
+import 'package:school_trip_app/utils/connectivity_observer.dart';
+
+class NetworkTestScreen extends StatefulWidget {
+  const NetworkTestScreen({super.key});
+
+  @override
+  State<NetworkTestScreen> createState() => _NetworkTestScreenState();
+}
+
+class _NetworkTestScreenState extends State<NetworkTestScreen> {
+  final ConnectivityObserver _connectivityOvserver =
+      NetworkConnectivityObserver();
+
+  var _status = Status.unavailable;
+
+  StreamSubscription<Status>? _subscription;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _subscription = _connectivityOvserver.observe().listen((status) {
+      setState(() {
+        _status = status;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('네크워크 상태 : ${_status.name}'),
+      ),
+    );
+  }
+}
