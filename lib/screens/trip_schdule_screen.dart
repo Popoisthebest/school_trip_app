7db557fe4c61defc0fce4bb073571901a4818a95
Popo <147984101+Popoisthebest@users.noreply.github.com>
2024-10-07 11:33:04 +0900

    [feat] 여행일정 Appbar 추가

--- lib/screens/trip_schdule_screen.dart
+++ lib/screens/trip_schdule_screen.dart
@@ -19,22 +19,42 @@ class _TripSchduleScreenState extends State<TripSchduleScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-      initialCameraPosition: CameraPosition(
-        target: _center,
-        zoom: 11.0,
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xff4D9E8A),
+        leading: const Column(
+          children: [
+            Text(
+              '도쿄 여행',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontFamily: 'Ownglyph okticon',
+                fontWeight: FontWeight.w700,
+                height: 0,
+              ),
+            )
+          ],
+        ),
       ),
-      markers: const {
-        // Marker(
-        //   markerId: const MarkerId("오사카시"),
-        //   position: const LatLng(34.6937249, 135.5022535),
-        //   infoWindow: InfoWindow(
-        //     title: "오사카시",
-        //     snippet: "우리가 갈 곳",
-        //     onTap: () {},
-        //   ), // InfoWindow
-        // ),
-      },
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
+        markers: const {
+          // Marker(
+          //   markerId: const MarkerId("오사카시"),
+          //   position: const LatLng(34.6937249, 135.5022535),
+          //   infoWindow: InfoWindow(
+          //     title: "오사카시",
+          //     snippet: "우리가 갈 곳",
+          //     onTap: () {},
+          //   ), // InfoWindow
+          // ),
+        },
+      ),
     );
   }
 }
