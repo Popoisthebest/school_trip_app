6f2f7b765ed5be439a525bd3da7a127d9a18b700
coder0104 <binbin080104@gmail.com>
2024-10-12 03:33:18 +0900

    [feat] place_screen 간격 배치

--- lib/screens/place_screens/place_screen.dart
+++ lib/screens/place_screens/place_screen.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class PlaceDetail {
   final String name;
   final String description;
-  final double rating;
+  final String rating;
   final String navigationText;
+  final String images;
+  final int starCount; // 별 개수 정보를 추가
 
   PlaceDetail({
+    required this.images,
     required this.name,
     required this.description,
     required this.rating,
     required this.navigationText,
+    required this.starCount, // 별 개수 매개변수 추가
   });
 }
 
@@ -19,159 +24,253 @@ class PlaceScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width; // 화면의 전체 너비를 계산합니다.
-    final screenHeight = MediaQuery.of(context).size.height; // 화면의 전체 높이를 계산합니다.
-    final cardWidth = screenWidth * 0.91; // 카드의 너비를 화면 너비의 91%로 설정합니다.
-    final cardHeight = screenHeight * 0.33; // 카드의 높이를 화면 높이의 33%로 설정합니다.
     final List<PlaceDetail> placeDetails = [
       PlaceDetail(
         name: '디즈니랜드',
-        description: '디즈니랜드 도쿄는 일본 최초의 디즈니 테마파크로, 다양한 디즈니 캐릭터와 놀이기구, 공연을 즐길 수 있는 인기 관광지입니다.',
-        rating: 5.0,
+        description:
+            '디즈니랜드 도쿄는 일본 최초의 디즈니 테마파크로, 다양한 디즈니 캐릭터와 놀이기구, 공연을 즐길 수 있는 인기 관광지입니다.',
+        rating: '5.0',
         navigationText: '길안내',
+        images: 'assets/place_screen_images/disneyland.png',
+        starCount: 5, // 별 개수 설정
       ),
       PlaceDetail(
-        name: '유니버설 스튜디오',
-        description: '유니버설 스튜디오 일본은 세계적으로 유명한 테마 파크로, 역동적인 어트랙션과 엔터테인먼트가 가득합니다.',
-        rating: 4.8,
+        name: '아키하바라 전...',
+        description:
+            '아키하바라 전자상가는 일본 도쿄에 위치한 전자제품과 애니메이션, 만화 관련 상품의 중심지로, 전 세계 오타구 문화의 성지로 불립니다.',
+        rating: '5.0',
         navigationText: '길안내',
+        images: 'assets/place_screen_images/akiabara.png',
+        starCount: 5, // 별 개수 설정
       ),
-      // 추가 장소 정보를 여기에 추가할 수 있습니다.
     ];
 
     return Scaffold(
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: Container(
-                decoration: const BoxDecoration(
-                  color: Color(0xFF4D9E8A),
+      body: Column(
+        children: [
+          // 상단 바
+          Container(
+            height: 172,
+            color: const Color(0xFF4D9E8A),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 68),
+                const Padding(
+                  padding: EdgeInsets.only(left: 29),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '도쿄 여행',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontFamily: 'Ownglyph okticon',
+                          fontWeight: FontWeight.w700,
+                          height: 1.0,
+                        ),
+                      ),
+                      SizedBox(height: 0),
+                      Text(
+                        '2024.10.22 ~ 10.25',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                          fontFamily: 'Ownglyph okticon',
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ),
-            Positioned(
-              left: 0,
-              top: 140,
-              right: 0,
-              child: Container(
-                height: 1,
-                color: const Color(0xFFE0E0E0),
-              ),
-            ),
-            Positioned(
-              left: 29,
-              top: 74,
-              child: const Text(
-                '도쿄 여행',
-                style: TextStyle(
+                const SizedBox(height: 7),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 1.0,
                   color: Colors.white,
-                  fontSize: 24,
-                  fontFamily: 'Ownglyph okticon',
-                  fontWeight: FontWeight.w700,
                 ),
-              ),
-            ),
-            Positioned(
-              left: 29,
-              top: 105,
-              child: Text(
-                '2024.10.22 ~ 10.25',
-                style: TextStyle(
-                  color: Colors.white.withOpacity(0.75),
-                  fontSize: 16,
-                  fontFamily: 'Ownglyph okticon',
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ),
-            Positioned(
-              left: 10,
-              top: 160,
-              right: 0,
-              child: SizedBox(
-                height: 40,
-                child: ListView(
+                const SizedBox(height: 14),
+                SingleChildScrollView(
                   scrollDirection: Axis.horizontal,
-                  children: [
-                    _buildCategoryButton('음식점'),
-                    const SizedBox(width: 10),
-                    _buildCategoryButton('공중화장실'),
-                    const SizedBox(width: 10),
-                    _buildCategoryButton('카페'),
-                    const SizedBox(width: 10),
-                    _buildCategoryButton('응급진료'),
-                    const SizedBox(width: 10),
-                    _buildCategoryButton('편의점'),
-                    const SizedBox(width: 10),
-                    _buildCategoryButton('은행'),
-                    const SizedBox(width: 10),
-                    _buildCategoryButton('가볼만한곳'),
-                  ],
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 10),
+                      _buildCategoryButton('음식점'),
+                      const SizedBox(width: 10),
+                      _buildCategoryButton('공중화장실'),
+                      const SizedBox(width: 10),
+                      _buildCategoryButton('카페'),
+                      const SizedBox(width: 10),
+                      _buildCategoryButton('응급진료'),
+                      const SizedBox(width: 10),
+                      _buildCategoryButton('편의점'),
+                      const SizedBox(width: 10),
+                      _buildCategoryButton('은행'),
+                      const SizedBox(width: 10),
+                      _buildCategoryButton('가볼만한곳'),
+                    ],
+                  ),
                 ),
-              ),
+                const SizedBox(height: 14),
+              ],
             ),
-            Positioned(
-              left: 0,
-              top: 220,
-              right: 0,
-              bottom: 0,
-              child: Container(
-                alignment: Alignment.center,
-                color: Colors.grey[300],
-                child: const Text('Map Placeholder'),
-              ),
-            ),
-            Positioned(
-              left: 0,
-              bottom: 0,
-              right: 0,
-              height: cardHeight,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: placeDetails.length,
-                padding: EdgeInsets.symmetric(horizontal: (screenWidth - cardWidth) / 2),
-                itemBuilder: (context, index) {
-                  return Container(
-                    width: cardWidth,
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(color: Color(0xFF4D9E8A), width: 2),
+          ),
+
+          // 중간 빨간색 박스와 하단 ListView를 분리하여 구성
+          Expanded(
+            child: Stack(
+              children: [
+                // 빨간색 박스
+                Positioned.fill(
+                  child: Container(
+                    color: const Color.fromARGB(255, 255, 0, 0),
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'Map Placeholder',
+                      style: TextStyle(color: Colors.white),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            placeDetails[index].name,
-                            style: const TextStyle(
-                              fontSize: 20,
-                              fontFamily: 'Ownglyph okticon',
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 4),
-                          Text(
-                            placeDetails[index].description,
-                            style: const TextStyle(
-                              fontFamily: 'Ownglyph okticon',
-                              fontSize: 14,
-                            ),
-                          ),
-                        ],
+                  ),
+                ),
+
+                // 하단 고정된 컴포넌트 (180px 고정 높이)
+                Positioned(
+                  bottom: 5, // 하단에서 5px 위로 띄움
+                  left: 6,
+                  right: 0,
+                  child: Container(
+                    height: 180,
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
                       ),
                     ),
-                  );
-                },
-              ),
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: placeDetails.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          width: 343,
+                          margin: const EdgeInsets.only(left: 10),
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                left: 0,
+                                top: 0,
+                                child: Container(
+                                  width: 343,
+                                  height: 200,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(30),
+                                    border: Border.all(
+                                      color: const Color(0xFF4D9E8A),
+                                      width: 2,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                left: 0,
+                                top: 0,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(10)
+                                    // topLeft: Radius.circular(10),
+                                    // topRight: Radius.circular(10),
+                                  ),
+                                  child: Image.asset(
+                                    placeDetails[index].images,
+                                    width: 120, // 이미지 넓이 설정
+                                    height: 180, // 이미지 높이 설정
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                left: 129,
+                                top: 27.9,
+                                child: SizedBox(
+                                  width: 145,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        placeDetails[index].name,
+                                        style: const TextStyle(
+                                          fontFamily: 'Ownglyph okticon',
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        placeDetails[index].description,
+                                        style: const TextStyle(
+                                          fontFamily: 'Ownglyph okticon',
+                                          fontSize: 12,
+                                          color: Color(0xBF1A1A1A),
+                                        ),
+                                      ),
+                                      Row(
+                                        children: [
+                                          // 별 아이콘을 개수만큼 생성
+                                          ...List.generate(
+                                            placeDetails[index].starCount,
+                                            (starIndex) => SvgPicture.asset(
+                                                'assets/place_screen_images/star_icon.svg', // SVG 파일 경로
+                                                width: 17, // 별 아이콘의 넓이
+                                                height: 17, // 별 아이콘의 높이
+                                              ),
+                                          ),
+                                          Text(
+                                            '(${placeDetails[index].rating})',
+                                            style: const TextStyle(
+                                              fontSize: 16,
+                                              color: Color(0xFF1A1A1A),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                right: 0,
+                                child: Container(
+                                  width: 60,
+                                  height: 180,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xFF4D9E8A),
+                                    borderRadius: BorderRadius.only(
+                                      topRight: Radius.circular(10),
+                                      bottomRight: Radius.circular(10),
+                                    ),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      placeDetails[index].navigationText,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
